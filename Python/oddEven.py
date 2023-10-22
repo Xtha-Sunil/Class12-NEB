be6ed16c0b385b3ca299f9bada8cc34ae5b117a8
luluw <shashinmhrzn@gmail.com>
2023-10-22 13:01:01 +0545

    Added Odd or Even

---
+++ Python/oddEven.py
@@ -0,0 +1,12 @@
+# User Input
+number = int(input("Enter a number: "))
+
+if number%2 == 0:
+    print(number,"is even")
+    
+else:
+    print(number,"is odd")
+
+# Ouptut:
+# Enter a number: 100
+# 100 is even
